688e79d23128d3d4d71bc8fc7bc768632bfbf301
KICKOFFMYBUTT <dhy_120@163.com>
2020-11-21 01:03:50 +0800

    added PC in pipeline registers

--- src/pipeline.v
+++ src/pipeline.v
@@ -48,6 +48,8 @@ module ID_EX (
     input wire clr,
     input wire [`WIDTH_INSTR-1:0] instr_ID,
     output wire [`WIDTH_INSTR-1:0] instr_EX,
+    input wire [31:0] PC_ID,
+    output wire [31:0] PC_EX,
     input wire [31:0] dataRs_ID,
     output wire [31:0] dataRs_EX,
     input wire [31:0] dataRt_ID,
@@ -66,6 +68,7 @@ module ID_EX (
     output wire [31:0] PCToLink_EX
 );
     reg [`WIDTH_INSTR-1:0] instr = 0;
+    reg [31:0] PC = 0;
     reg [31:0] dataRs = 0;
     reg [31:0] dataRt = 0;
     reg [15:0] imm16 = 0;
@@ -76,6 +79,7 @@ module ID_EX (
     reg [31:0] PCToLink = 0;
 
     assign instr_EX = instr;
+    assign PC_EX = PC;
     assign dataRs_EX = dataRs;
     assign dataRt_EX = dataRt;
     assign imm16_EX = imm16;
@@ -88,6 +92,7 @@ module ID_EX (
     always @ (posedge clk) begin
         if (reset | clr) begin
             instr <= 0;
+            PC <= 0;
             dataRs <= 0;
             dataRt <= 0;
             imm16 <= 0;
@@ -100,6 +105,7 @@ module ID_EX (
         else begin
             if (!stall) begin
                 instr <= instr_ID;
+                PC <= PC_ID;
                 dataRs <= dataRs_ID;
                 dataRt <= dataRt_ID;
                 imm16 <= imm16_ID;
@@ -122,6 +128,8 @@ module EX_MEM (
     input wire clr,
     input wire [`WIDTH_INSTR-1:0] instr_EX,
     output wire [`WIDTH_INSTR-1:0] instr_MEM,
+    input wire [31:0] PC_EX,
+    output wire [31:0] PC_MEM,
     input wire [31:0] aluOut_EX,
     output wire [31:0] aluOut_MEM,
     input wire [31:0] memWriteData_EX,
@@ -132,12 +140,14 @@ module EX_MEM (
     output wire [31:0] PCToLink_MEM
 );
     reg [`WIDTH_INSTR-1:0] instr = 0;
+    reg [31:0] PC = 0;
     reg [31:0] aluOut = 0;
     reg [31:0] memWriteData = 0;
     reg [4:0] regWriteAddr = 0;
     reg [31:0] PCToLink = 0;
 
     assign instr_MEM = instr;
+    assign PC_MEM = PC;
     assign aluOut_MEM = aluOut;
     assign memWriteData_MEM = memWriteData;
     assign regWriteAddr_MEM = regWriteAddr;
@@ -146,6 +156,7 @@ module EX_MEM (
     always @ (posedge clk) begin
         if (reset | clr) begin
             instr <= 0;
+            PC <= 0;
             aluOut <= 0;
             memWriteData <= 0;
             regWriteAddr <= 0;
@@ -154,6 +165,7 @@ module EX_MEM (
         else begin
             if (!stall) begin
                 instr <= instr_EX;
+                PC <= PC_EX;
                 aluOut <= aluOut_EX;
                 memWriteData <= memWriteData_EX;
                 regWriteAddr <= regWriteAddr_EX;
@@ -170,6 +182,8 @@ module MEM_WB (
     input wire reset,
     input wire stall, 
     input wire clr,
+    input wire [31:0] PC_MEM,
+    output wire [31:0] PC_WB,
     input wire [`WIDTH_INSTR-1:0] instr_MEM,
     output wire [`WIDTH_INSTR-1:0] instr_WB,
     input wire [31:0] aluOut_MEM,
@@ -181,12 +195,14 @@ module MEM_WB (
     input wire [4:0] regWriteAddr_MEM,
     output wire [4:0] regWriteAddr_WB
 );
+    reg [31:0] PC = 0;
     reg [`WIDTH_INSTR-1:0] instr = 0;
     reg [31:0] aluOut = 0;
     reg [31:0] memReadData = 0;
     reg [31:0] PCToLink = 0;
     reg [4:0] regWriteAddr = 0;
 
+    assign PC_WB = PC;
     assign instr_WB = instr;
     assign aluOut_WB = aluOut;
     assign memReadData_WB = memReadData;
@@ -195,6 +211,7 @@ module MEM_WB (
 
     always @ (posedge clk) begin
         if (reset | clr) begin
+            PC <= 0;
             instr <= 0;
             aluOut <= 0;
             memReadData <= 0;
@@ -203,6 +220,7 @@ module MEM_WB (
         end 
         else begin
             if (!stall) begin
+                PC <= PC_MEM;
                 instr <= instr_MEM;
                 aluOut <= aluOut_MEM;
                 memReadData <= memReadData_MEM;
