f5d09fcd8531e9beac2824950ca416ab47d999c1
dhy2000 <dhy_120@163.com>
2020-12-15 00:59:19 +0800

    work

--- src/CPU/MEM/CP0.v
+++ src/CPU/MEM/CP0.v
@@ -7,10 +7,86 @@
 
 `include "../instructions.v"
 `include "../IC.v"
+`include "../../memconfig.v"
 
 module CP0 (
     input wire clk, 
-    input wire reset
+    input wire reset,
+    input wire [31:0] PC, 
+    input wire [31:0] WData, 
+    input wire [4:0] CP0id,     // addrRd
+    input wire [`WIDTH_INSTR-1:0] instr, 
+    input wire [`WIDTH_INSTR-1:0] instr_WB, // to check delay slot
+    // Interrupt and Exception Control
+    input wire [7:2] HWInt, 
+    input wire [6:2] Exc,
+    output wire ExcEntry,       // control signal send to NPC@IF
+    output reg [31:2] EPC = (`TEXT_STARTADDR >> 2), 
+    output wire [31:0] RData
 );
+parameter   idSR    = 12, 
+            idCause = 13,
+            idEPC   = 14,
+            idPrID  = 15;
+    // SR, Cause, PrID
+    // SR
+    reg [7:2] IM = 6'b111111;
+    reg EXL = 0, IE = 1;
+    wire [31:0] SR = {16'b0, IM, 8'b0, EXL, IE};
+    // Cause
+    reg [7:2] IP = 6'b000000;
+    reg [6:2] ExcCode = 6'b000000;
+    reg BD = 0;
+    wire [31:0] Cause = {BD, 15'b0, IP, 3'b0, ExcCode, 2'b0};
+    // PrID
+    reg [31:0] PrID = 32'hbaad_face;
+
     
+    // Interrupt Handler
+    wire Interrupt;
+    assign Interrupt = (IM[7:2] & IP[7:2]) & IE & (!EXL);
+
+    // support MFC0, MTC0, ERET
+    // MFC0 - Read
+    assign RData = (instr == `MFC0) ? (
+        (CP0id == idSR) ? (SR) : 
+        (CP0id == idCause) ? (Cause) : 
+        (CP0id == idEPC) ? (EPC) : 
+        (CP0id == idPrID) ? (PrID) : 
+        0
+    ) : 0;
+
+    // 
+
+
+    always @ (posedge clk) begin
+        if (reset) begin
+            EXL <= 0;
+            IE <= 1;
+            IM <= 6'b111111;
+            IP <= 0;
+            ExcCode <= 0;
+            BD <= 0;
+            EPC <= (`TEXT_STARTADDR >> 2);
+        end
+        else begin
+            // SR
+            if (instr == `MTC0 && CP0id == idSR) 
+                {IM, EXL, IE} <= {WData[15:10], WData[1], WData[0]};
+            // Cause
+            IP <= HWInt;
+            ExcCode <= Exc;
+
+
+            // EPC
+            if (Interrupt || ExcCode) begin
+                
+            end
+
+            // PrID
+            // cannot write
+        end
+    end
+
+
 endmodule
