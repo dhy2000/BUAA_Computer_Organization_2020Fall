af623b8a4ca4ae3b05b4d3734461e032324a7fc5
dhy2000 <dhy_120@163.com>
2020-12-15 21:31:23 +0800

    upgraded HazardUnit to PipelineControl

--- src/CPU/PipelineControl.v
+++ src/CPU/PipelineControl.v
@@ -82,25 +82,44 @@ module InstrTuseTnew (
 
 endmodule
 
-module HazardUnit (
+module PipelineControl (
+    input wire [31:0] PC_IF,
+    input wire [31:0] PC_ID, 
+    input wire [31:0] PC_EX, 
+    input wire [31:0] PC_MEM, 
+    input wire [31:0] PC_WB, 
     input wire [`WIDTH_INSTR-1:0] instr_ID, 
     input wire [`WIDTH_INSTR-1:0] instr_EX,
     input wire [`WIDTH_INSTR-1:0] instr_MEM, 
+    input wire [`WIDTH_INSTR-1:0] instr_WB, 
+    /* Hazard Stall */
     input wire [4:0] addrRs_ID, // use
     input wire [4:0] addrRt_ID, // use
     input wire [4:0] regWriteAddr_EX,  // new
     input wire [4:0] regWriteAddr_MEM, // new
     input wire [`WIDTH_T-1:0] Tnew_EX, 
     input wire [`WIDTH_T-1:0] Tnew_MEM,
-    // input wire [`WIDTH_T-1:0] Tnew_WB, 
     input wire MDBusy,     // multdiv unit
+    /* Control by CP0 */
+    input wire [`WIDTH_KCTRL-1:0] KCtrl_CP0, 
     // Output Tnew to pipeline
     output wire [`WIDTH_T-1:0] Tnew_ID, 
-    // Output Stall Signals
+    // Macro PC
+    output wire [31:0] MacroPC, 
+    // Output Pipeline Control Signals
     output wire stall_PC, 
     output wire stall_ID, 
-    output wire clr_EX
+    output wire clr_ID, 
+    output wire clr_EX, 
+    output wire clr_MEM, 
+    output wire clr_WB, 
+    // Disable Function Parts to prevent write
+    output wire dis_MULTDIV, 
+    output wire dis_DM, 
+    // NPC Ctrl
+    output wire [`WIDTH_KCTRL-1:0] KCtrl_NPC
 );
+    /* Part 1. Pipeline Stall */
     wire [`WIDTH_FUNC-1:0] func_ID;
     wire [`WIDTH_T-1:0] Tuse_rs, Tuse_rt;
     InstrTuseTnew tusetnew (
@@ -124,8 +143,32 @@ module HazardUnit (
     wire stall_md;
     assign stall_md = (MDBusy) && (func_ID == `FUNC_MULTDIV);
 
-    // TODO: mult/div stalls
+    wire stall_stallPC, stall_clrEX, stall_stallID;
 
-    assign {stall_PC, stall_ID, clr_EX} = (stall_rs || stall_rt || stall_md) ? 3'b111 : 3'b000;
+    assign {stall_stallPC, stall_stallID, stall_clrEX} = (stall_rs || stall_rt || stall_md) ? 3'b111 : 3'b000;
+
+    /* Part 2. Interrupt and Exception by CP0 */
+    wire cp0_clrEX;
+    wire flushAll = (KCtrl_CP0 == `KCTRL_KTEXT || KCtrl_CP0 == `KCTRL_ERET);
+    assign KCtrl_NPC = KCtrl_CP0;
+
+    assign {clr_ID, cp0_clrEX, clr_MEM, clr_WB} = (flushAll) ? 4'b1111 : 4'b0000;
+
+
+    /* Part 3. Macro PC */
+    // instantiate an IC
+    wire [`WIDTH_FUNC-1:0] func_WB;
+    IC ic (.instr(instr_WB), .format(), .func(func_WB));
+
+    assign MacroPC = ((func_WB == `FUNC_BRANCH) || (func_WB == `FUNC_JUMP)) ? (PC_WB) :  // Branch Delay Slot
+                        (PC_MEM) ? (PC_MEM) : 
+                        (PC_EX) ? (PC_EX) : 
+                        (PC_ID) ? (PC_ID) : 
+                        (PC_IF) ? (PC_IF) : 0;
+    /* Merge output signals */
+    assign stall_PC = (!flushAll) && (stall_stallPC);
+    assign stall_ID = (!flushAll) && (stall_stallID);
+    assign clr_EX = stall_clrEX | cp0_clrEX; // Two things controlls clearing ID/EX pipeline register
+
 
 endmodule
