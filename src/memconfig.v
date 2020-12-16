875a7ff3d6bbc6da22c6991f6381eb0eddaac6ab
dhy2000 <dhy_120@163.com>
2020-12-16 10:46:41 +0800

    added macros of end-address

--- src/memconfig.v
+++ src/memconfig.v
@@ -12,6 +12,7 @@
 `define WIDTH_DM_ADDR       (`WIDTH_DM_ADDR_WORD + 2)
 `define DM_SIZE_WORD        (1 << `WIDTH_DM_ADDR_WORD)
 `define DM_SIZE             (`DM_SIZE_WORD << 2)
+`define DATA_ENDADDR        (`DATA_STARTADDR + `DM_SIZE - 4)
 
 /* ------ TEXT MEMORY ------ */
 `define TEXT_STARTADDR  32'h0000_3000
@@ -19,6 +20,7 @@
 `define WIDTH_IM_ADDR       (`WIDTH_IM_ADDR_WORD + 2)
 `define IM_SIZE_WORD        (1 << `WIDTH_IM_ADDR_WORD)
 `define IM_SIZE             (`IM_SIZE_WORD << 2)
+`define TEXT_ENDADDR        (`TEXT_ENDADDR + `IM_SIZE - 4)
 /* ------ Exception Handler ------ */
 `define KTEXT_STARTADDR 32'h0000_4180
 `define KTEXT_ENDADDR   32'h0000_4ffc
