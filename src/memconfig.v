01b3b5f96acb525826d6be670dd9b496ed793d16
dhy2000 <dhy_120@163.com>
2020-12-22 16:32:53 +0800

    reduced memory size

--- src/memconfig.v
+++ src/memconfig.v
@@ -8,15 +8,15 @@
 
 /* ------ DATA MEMORY ------ */
 `define DATA_STARTADDR      32'h0000_0000
-`define WIDTH_DM_ADDR_WORD  12  // 12: 4096 * 4 Bytes
+`define WIDTH_DM_ADDR_WORD  10  // 10: 1024 * 4 Bytes
 `define WIDTH_DM_ADDR       (`WIDTH_DM_ADDR_WORD + 2)
 `define DM_SIZE_WORD        (1 << `WIDTH_DM_ADDR_WORD)
 `define DM_SIZE             (`DM_SIZE_WORD << 2)
-`define DATA_ENDADDR        32'h0000_3000 // [start, end)
+`define DATA_ENDADDR        32'h0000_1000 // [start, end)
 
 /* ------ TEXT MEMORY ------ */
 `define TEXT_STARTADDR      32'h0000_3000
-`define WIDTH_IM_ADDR_WORD  12  // 12: 4096 * 4 Bytes
+`define WIDTH_IM_ADDR_WORD  11  // 12: 2048 * 4 Bytes
 `define WIDTH_IM_ADDR       (`WIDTH_IM_ADDR_WORD + 2)
 `define IM_SIZE_WORD        (1 << `WIDTH_IM_ADDR_WORD)
 `define IM_SIZE             (`IM_SIZE_WORD << 2)
