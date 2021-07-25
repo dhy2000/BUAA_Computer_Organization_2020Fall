59ba3cdf8e55861aa7cfc8690404bd1cc273066d
dhy2000 <dhy_120@163.com>
2021-07-26 03:22:45 +0800

    refactor PC

--- src/StageIF.v
+++ src/StageIF.v
@@ -1,7 +1,5 @@
 /* 
- *  File Name: StageIF.v
- *  Modules: NPC, PC, IM
- *  Description: Top module of pipeline stage IF
+ *  Overview: Pipeline Stage F (instruction Fetch)
  */
 
 `default_nettype none
@@ -9,119 +7,92 @@
 `include "include/memconfig.v"
 `include "include/exception.v"
 
+/*
+ *  Overview: Program Counter
+ *  Inputs: Branch/Jump from D, exception/eret from CP0
+ */
 module PC (
     input wire clk, 
     input wire reset,
-    // control
-    input wire En,  // Write Enable, 0 if the pipeline stalls
-    // data
-    input wire [31:0] NPC, // next PC
-    // output
-    output wire [31:0] PC,
-    // exception flag
-    output wire [6:2] exc
-);
-    reg [31:0] pc = `IM_ADDR_START;
-    assign PC = pc;
-
-    // Exception
-    assign exc = ((!(PC >= `IM_ADDR_START && PC < `IM_ADDR_END)) || (PC[1:0] != 0)) ? (`EXC_ADEL) : 0;
-    
-    initial begin
-        pc <= `IM_ADDR_START;
-    end
-    
-    always @(posedge clk ) begin
-        if (reset) begin
-            pc <= `IM_ADDR_START;
-        end
-        else begin
-            if (En) begin
-                pc <= NPC;
-            end
-            else begin
-                pc <= pc;
-            end
-        end
-    end
-
-endmodule
-
-module NPC (
-    input wire [`WIDTH_INSTR-1:0] instr,
+    // Pipeline Control
+    input wire en,
+    // D stage
+    input wire `TYPE_INSTR instr,
+    input wire `TYPE_IFUNC ifunc,
     input wire cmp,
-    input wire [31:0] PC,
     input wire [15:0] imm16,
     input wire [25:0] jmpAddr,
     input wire [31:0] jmpReg,
-    /* Control by CP0 */
-    input wire [`WIDTH_ECTRL-1:0] KCtrl, 
-    input wire [31:2] EPC, 
-    output wire [31:0] NPC,
-    output wire isJmp
+    // Exception
+    input wire `TYPE_ECTRL excCtrl,
+    input wire [31:2] EPC,
+    // output
+    output reg [31:0] PC = `PC_BOOT,
+    output wire BD,
+    output wire `TYPE_EXC excCode
 );
-    parameter WIDTH_NPC = 2,
-            NPC_Order   = 0,
-            NPC_Branch  = 1,
-            NPC_JmpImm  = 2,
-            NPC_JmpReg  = 3;
-    // control
-    // instantiate ic module
-    wire `TYPE_FORMAT format; wire `TYPE_IFUNC ifunc;
-    IC ic (.instr(instr), .format(format), .ifunc(ifunc));
-    wire [WIDTH_NPC-1:0] npcOp;
+    localparam
+        NPC_Order   = 0,
+        NPC_Branch  = 1,
+        NPC_JmpImm  = 2,
+        NPC_JmpReg  = 3,
+        NPC_ExcEnt  = 4,
+        NPC_ExcRet  = 5,
+        NPC_Keep    = 6;
+    
+    // BD Flag
+    assign BD = (ifunc == `I_BRANCH || ifunc == `I_JUMP); // no matter whether really branch
+
+    // Extend Immediate
+    wire `WORD extImm, extJmp;
+    assign extImm = {{14{imm16[15]}}, imm16, 2'b0};
+    assign extJmp = {PC[31:28], jmpAddr, 2'b0};
+
+    // Termination to avoid pc out of range without ending infinite loop
+    wire terminated;    // reserved
+    assign terminated = (PC == `KTEXT_START - 4);
+
+    // NPC Control
+    wire [3 : 0] npcOp;
     assign npcOp = (
+        (excCtrl == `E_ENTRY) ? (NPC_ExcEnt) : 
+        (excCtrl == `E_ERET) ? (NPC_ExcRet) :
+        (terminated) ? (NPC_Keep) :
         (instr == `JALR || instr == `JR) ? (NPC_JmpReg) : 
         (instr == `J    || instr == `JAL) ? (NPC_JmpImm) : 
         ((ifunc == `I_BRANCH) && cmp) ? (NPC_Branch) : 
         (NPC_Order)
     );
-
-    assign isJmp = (ifunc == `I_BRANCH || ifunc == `I_JUMP);
-
-    wire [31:0] extImm, extJmp;
-    assign extImm = {{14{imm16[15]}}, imm16, 2'b0};
-    assign extJmp = {PC[31:28], jmpAddr, 2'b0};
-
-    wire terminated;
-    assign terminated = (PC == `KTEXT_START - 4);
-
-    assign NPC = (KCtrl == `E_ENTRY) ? (`KTEXT_START) : 
-                (KCtrl == `E_ERET) ? ({EPC[31:2], 2'b0}) : 
-                (terminated) ? (PC) : 
-                (npcOp == NPC_Order) ? (PC + 4) : 
+    
+    // Select data source
+    wire [31 : 0] NPC;
+    assign NPC = (npcOp == NPC_Order) ? (PC + 4) : 
                 (npcOp == NPC_Branch) ? (PC + extImm) : // This PC is After b/j
                 (npcOp == NPC_JmpImm) ? (extJmp) : 
                 (npcOp == NPC_JmpReg) ? (jmpReg) : 
-                (PC + 4);
-
-endmodule
-
-module IM (
-    input wire [31:0] PC,
-    output wire [31:0] code
-);
-    // Memory
-    reg [31:0] mem [0: `IM_WORDNUM - 1];
-    wire [31:0] baseAddr;
-    assign baseAddr = PC - `IM_ADDR_START;
-    wire [`IM_ADDR_WIDTH-1:2] wordIndex;
-    assign wordIndex = baseAddr[`IM_ADDR_WIDTH-1:2];
-
-    wire [31:0] memword;
-    assign memword = (PC >= `IM_ADDR_START && PC < (`IM_ADDR_START + `IM_SIZE)) ? mem[wordIndex] : 0;
-
-    assign code = (^memword === 1'bx) ? 0 : memword;
-
-    initial begin
-        $readmemh(`CODE_FILE, mem);
-        // $readmemh(`HANDLER_FILE, mem, 
-        //     ((`KTEXT_START - `IM_ADDR_START) >> 2), 
-        //     ((`KTEXT_END - `IM_ADDR_START) >> 2)
-        // );
-    end
-
+                (npcOp == NPC_ExcEnt) ? (`KTEXT_START) : 
+                (npcOp == NPC_ExcRet) ? ({EPC[31:2], 2'b0}) :
+                (PC);
     
+    // State update
+    initial begin
+        PC <= `PC_BOOT;
+    end
+    
+    always @(posedge clk) begin
+        if (reset) begin
+            PC <= `PC_BOOT;
+        end
+        else begin
+            if (en) begin
+                PC <= NPC;
+            end
+        end
+    end
+    
+    // Exception: Out of IM range or Not Aligned by Word
+    assign excCode = ((!(PC >= `IM_ADDR_START && PC < `IM_ADDR_END)) || (PC[1:0] != 0)) ? (`EXC_ADEL) : 0;
+
 endmodule
 
 module StageIF (
