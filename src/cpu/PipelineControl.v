53433dc767e3152d40f0ab6232e71dcd921dc8c7
dhy2000 <dhy_120@163.com>
2021-07-27 05:59:44 +0800

    done pipeline control

--- src/cpu/PipelineControl.v
+++ src/cpu/PipelineControl.v
@@ -1,197 +1,121 @@
 /*
- *  File Name: PipelineControl.v
- *  Module: PipelineControl
- *  Description: Control the stalls and flushes of Pipeline(Forward control is inner the level top module)
+ *  Overview: Pipeline Controller
  */
+
 `default_nettype none
-`include "include/instructions.v"
-`include "include/exception.v"
+`include "../include/instructions.v"
+`include "../include/exception.v"
+`include "../include/memory.v"
 
-module InstrTuseTnew (
-    input wire `TYPE_INSTR instr, 
-    output wire `TYPE_IFUNC ifunc, 
-    output wire [`WIDTH_T-1:0] Tuse_rs, 
-    output wire [`WIDTH_T-1:0] Tuse_rt,
-    output wire [`WIDTH_T-1:0] Tnew_ID     // Tnew @ ID
-);
-    wire `TYPE_FORMAT format; // wire `TYPE_IFUNC ifunc;
-    IC ic (.instr(instr), .format(format), .ifunc(ifunc));
-
-    assign Tuse_rs = (
-        (instr == `MOVN || instr == `MOVZ) ? 0 : 
-        // Calc_R
-        (ifunc == `I_ALU_R) ? (
-            ((instr == `SLL) || (instr == `SRL) || (instr == `SRA)) ? (`TUSE_INF) : 1
-        ) : 
-        (ifunc == `I_ALU_I) ? (
-            ((instr == `LUI)) ? (`TUSE_INF) : 1
-        ) : 
-        (ifunc == `I_MEM_R) ? 1 : 
-        (ifunc == `I_MEM_W) ? 1 : 
-        (ifunc == `I_BRANCH) ? 0 : 
-        (ifunc == `I_JUMP) ? (
-            ((instr == `JR) || (instr == `JALR)) ? 0 : (`TUSE_INF)
-        ) : 
-        (ifunc == `I_MD) ? (
-            ((instr == `MULT) || (instr == `MULTU) || (instr == `DIV) || (instr == `DIVU)) ? 1 : 
-            ((instr == `MTHI) || (instr == `MTLO)) ? 1 : 
-            (`TUSE_INF)
-        ) : 
-        (ifunc == `I_CP0) ? (`TUSE_INF) : 
-        (`TUSE_INF)
-    );
-    assign Tuse_rt = (
-        (instr == `MOVN || instr == `MOVZ) ? 0 : 
-        (ifunc == `I_ALU_R) ? (
-            ((instr == `CLO) || (instr == `CLZ)) ? (`TUSE_INF) : 
-            1
-        ) : 
-        (ifunc == `I_ALU_I) ? (`TUSE_INF) : 
-        (ifunc == `I_MEM_R) ? (`TUSE_INF) : 
-        (ifunc == `I_MEM_W) ? 2 : 
-        (ifunc == `I_BRANCH) ? (
-            ((instr == `BEQ) || (instr == `BNE)) ? 0 : (`TUSE_INF)
-        ) : 
-        (ifunc == `I_JUMP) ? (`TUSE_INF) : 
-        (ifunc == `I_MD) ? (
-            ((instr == `MULT) || (instr == `MULTU) || (instr == `DIV) || (instr == `DIVU)) ? 1 : 
-            (`TUSE_INF)
-        ) : 
-        (ifunc == `I_CP0) ? (
-            (instr == `MTC0) ? 2 : (`TUSE_INF)
-        ) : 
-        (`TUSE_INF)
-    );
-
-    assign Tnew_ID = (
-        (instr == `MOVZ || instr == `MOVN) ? 1 : 
-        (ifunc == `I_ALU_R) ? 2 : 
-        (ifunc == `I_ALU_I) ? (
-            (instr == `LUI) ? 1 : 2
-        ) : 
-        (ifunc == `I_MEM_R) ? (
-            (instr == `LW) ? 3 : 5
-        ) : 
-        (ifunc == `I_MEM_W) ? 0 : 
-        (ifunc == `I_BRANCH) ? 0 : 
-        (ifunc == `I_JUMP) ? (
-            ((instr == `JAL) || (instr == `JALR)) ? 1 : 0
-        ) : 
-        (ifunc == `I_MD) ? (
-            ((instr == `MFLO) || (instr == `MFHI)) ? 2 : 0
-        ) : 
-        (ifunc == `I_CP0) ? (
-            (instr == `MFC0) ? 3 : 0
-        ) : 
-        0   // NOP
-    );
-
-endmodule
+/*
+ *  Overview: Pipeline controller module, controlling the pipeline work or stall, also processing macro status of cpu.
+ *  Input:
+ *      - Instruction at D
+ *      - Register use of each stage
+ *      - Register write of each stage
+ *      - Tnew at each stage
+ *      - Busy signals of each stage
+ *      - EXL Op from CP0
+ *      -
+ *  Output:
+ *      - Stall signals
+ *      - Clear signals
+ *      - Enable signals for some stage
+ */
 
 module PipelineControl (
-    input wire [31:0] PC_IF,
-    input wire [31:0] PC_ID, 
-    input wire [31:0] PC_EX, 
-    input wire [31:0] PC_MEM, 
-    input wire [31:0] PC_WB, 
-    input wire `TYPE_INSTR instr_ID, 
-    input wire `TYPE_INSTR instr_EX,
-    input wire `TYPE_INSTR instr_MEM, 
-    input wire `TYPE_INSTR instr_WB, 
-    input wire BD_IF, 
-    input wire BD_ID, 
-    input wire BD_EX, 
-    input wire BD_MEM, 
-    // Exception Code on every stage
-    input wire [6:2] Exc_ID, 
-    input wire [6:2] Exc_EX,
-    input wire [6:2] Exc_MEM, 
-    /* Hazard Stall */
-    input wire [4:0] addrRs_ID, // use
-    input wire [4:0] addrRt_ID, // use
-    input wire [4:0] regWriteAddr_EX,  // new
-    input wire [4:0] regWriteAddr_MEM, // new
-    input wire [`WIDTH_T-1:0] Tnew_EX, 
-    input wire [`WIDTH_T-1:0] Tnew_MEM,
-    input wire MDBusy,     // multdiv unit
-    /* Control by CP0 */
-    input wire [`WIDTH_EXLOP-1:0] KCtrl_CP0, 
-    input wire BD_CP0, 
-    // Output Tnew to pipeline
-    output wire [`WIDTH_T-1:0] Tnew_ID, 
-    // Macro PC
-    output wire [31:0] MacroPC, 
-    output wire MacroBD, // BD flag sync with MacroPC
-    // Output Pipeline Control Signals
-    output wire stall_PC, 
-    output wire stall_ID, 
-    output wire clr_ID, 
-    output wire clr_EX, 
-    output wire clr_MEM, 
-    output wire clr_WB, 
-    // Disable Function Parts to prevent write
-    output wire dis_MULTDIV, 
-    output wire dis_DM, 
-    output wire dis_GRF, 
-    // NPC Ctrl
-    output wire [`WIDTH_EXLOP-1:0] KCtrl_NPC
+    // instruction at D
+    input wire `TYPE_INSTR instr_D,
+    input wire `TYPE_IFUNC ifunc_D,
+    // reg use
+    input wire useRs_D,
+    input wire useRt_D,
+    input wire `TYPE_REG addrRs_D,
+    input wire `TYPE_REG addrRt_D,
+    input wire useRs_E,
+    input wire useRt_E,
+    input wire `TYPE_REG addrRs_E,
+    input wire `TYPE_REG addrRt_E,
+    input wire useRt_M,
+    input wire `TYPE_REG addrRt_M,
+    // reg write
+    input wire regWEn_D,
+    input wire `TYPE_REG regWAddr_D,
+    input wire regWValid_D,
+    input wire regWEn_E,
+    input wire `TYPE_REG regWAddr_E,
+    input wire regWValid_E,
+    input wire regWEn_M,
+    input wire `TYPE_REG regWAddr_M,
+    input wire regWValid_M,
+    input wire regWEn_W,
+    input wire `TYPE_REG regWAddr_W,
+    input wire regWValid_W,
+    // Tnew
+    input wire `TYPE_T TuseRs_D,
+    input wire `TYPE_T TuseRt_D,
+    input wire `TYPE_T Tnew_D,
+    input wire `TYPE_T Tnew_E,
+    input wire `TYPE_T Tnew_M,
+    input wire `TYPE_T Tnew_W,
+    // busy
+    input wire busyI,
+    input wire busyD,
+    input wire busyMD,
+    // EXL
+    input wire `TYPE_EXLOP EXLOp,
+    // output
+    output wire enPC,
+    output wire enMD,
+    output wire enD,
+    output wire stall_D,
+    output wire clear_D,
+    output wire stall_E,
+    output wire clear_E,
+    output wire stall_M,
+    output wire clear_M,
+    output wire stall_W,
+    output wire clear_W
 );
-    /* Part 1. Pipeline Stall */
-    wire `TYPE_IFUNC ifunc_ID;
-    wire [`WIDTH_T-1:0] Tuse_rs, Tuse_rt;
-    InstrTuseTnew tusetnew (
-        .instr(instr_ID),
-        .ifunc(ifunc_ID),
-        .Tuse_rs(Tuse_rs),
-        .Tuse_rt(Tuse_rt),
-        .Tnew_ID(Tnew_ID)
+
+    /*
+     *  Pipeline stall conditions:
+     *      1. I-Fetch busy: @F, disable PC, stall F/D, clear D/E
+     *      2. Data hazard (Tnew > Tuse): @D, disable PC, stall F/D, clear D/E
+     *      3. MDU busy with MDU instr following: @D, disable PC, stall F/D, clear D/E
+     *      4. D-RW busy: @M, disable PC, stall F/D, stall D/E, stall E/M, clear M/W
+     *      5. EXL change (Interrupt/Exception, eret): enable PC, clear all (HIGHEST PRIORITY)
+     */
+
+    // 1. I-Fetch busy
+        // busyI
+    
+    // 2. Data hazard
+    wire waitRs, waitRt;
+    assign waitRs = (useRs_D & (addrRs_D != 0)) && (
+        ((regWEn_E) & (regWAddr_E == addrRs_D) & (Tnew_E > TuseRs_D)) ||
+        ((regWEn_M) & (regWAddr_M == addrRs_D) & (Tnew_M > TuseRs_D))
     );
-
-    wire stall_rs, stall_rt;
-    assign stall_rs = (addrRs_ID != 0) && (
-        (Tnew_EX > Tuse_rs && regWriteAddr_EX == addrRs_ID) || 
-        (Tnew_MEM > Tuse_rs && regWriteAddr_MEM == addrRs_ID)
+    assign waitRt = (useRt_D & (addrRt_D != 0)) && (
+        ((regWEn_E) & (regWAddr_E == addrRt_D) & (Tnew_E > TuseRt_D)) ||
+        ((regWEn_M) & (regWAddr_M == addrRt_D) & (Tnew_M > TuseRt_D))
     );
-    assign stall_rt = (addrRt_ID != 0) && (
-        (Tnew_EX > Tuse_rt && regWriteAddr_EX == addrRt_ID) || 
-        (Tnew_MEM > Tuse_rt && regWriteAddr_MEM == addrRt_ID)
-    );
+    wire waitReg = (waitRs | waitRt);
 
-    wire stall_md;
-    assign stall_md = (MDBusy) && (ifunc_ID == `I_MD);
+    // 3. MDU busy
+    wire waitMD = (busyMD & (ifunc_D == `I_MD));
 
-    wire stall_stallPC, stall_clrEX, stall_stallID;
+    // 4. D-RW busy
+        // busyD
 
-    assign {stall_stallPC, stall_stallID, stall_clrEX} = (stall_rs || stall_rt || stall_md) ? 3'b111 : 3'b000;
+    // 5. EXL change
+    wire exl = (EXLOp != 0);
 
-    /* Part 2. Interrupt and Exception by CP0 */
-    wire cp0_clrEX;
-    wire flushAll = (KCtrl_CP0 == `EXL_ENTRY || KCtrl_CP0 == `EXL_ERET);
-    assign KCtrl_NPC = KCtrl_CP0;
-    assign {dis_MULTDIV, dis_DM} = (flushAll)? 2'b11 : 2'b00;
-    // assign dis_GRF = (flushAll && BD_CP0) ? 1'b1 : 1'b0;
-    assign dis_GRF = 0;
-    assign {clr_ID, cp0_clrEX, clr_MEM, clr_WB} = (flushAll) ? 4'b1111 : 4'b0000;
-
-
-    /* Part 3. Macro PC */
-    // instantiate an IC
-    wire `TYPE_IFUNC ifunc_WB;
-    IC ic (.instr(instr_WB), .format(), .ifunc(ifunc_WB));
-
-    assign MacroPC = (PC_MEM || Exc_MEM) ? (PC_MEM) : 
-                        (PC_EX || Exc_EX) ? (PC_EX) : 
-                        (PC_ID || Exc_ID) ? (PC_ID) : 
-                        (PC_IF) ? (PC_IF) : 0;
-    assign MacroBD = (PC_MEM || Exc_MEM) ? (BD_MEM) : 
-                        (PC_EX || Exc_EX) ? (BD_EX) : 
-                        (PC_ID || Exc_ID) ? (BD_ID) : 
-                        (PC_IF) ? (BD_IF) : 0;
-
-    /* Merge output signals */
-    assign stall_PC = (!flushAll) && (stall_stallPC);
-    assign stall_ID = (!flushAll) && (stall_stallID);
-    assign clr_EX = stall_clrEX | cp0_clrEX; // Two things controlls clearing ID/EX pipeline register
-
+    // Generate control signals
+    assign {enPC, enMD, enD, stall_D, stall_E, stall_M, stall_W, clear_D, clear_E, clear_M, clear_W} = 
+        (exl)                               ? (11'b100_0000_1111) :
+        (busyD)                             ? (11'b011_1110_0001) :
+        (waitMD | waitReg | busyI)          ? (11'b011_1000_0100) :
+        (11'b111_0000_0000);
 
 endmodule
