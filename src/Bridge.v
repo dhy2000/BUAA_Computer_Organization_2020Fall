ca2b085d1837c232c8316566698729427d4aab13
dhy2000 <dhy_120@163.com>
2021-07-27 14:59:35 +0800

    done bridge

--- src/Bridge.v
+++ src/Bridge.v
@@ -1,113 +1,107 @@
-/* 
- * File Name: Bridge.v
- * Module Name: NorthBridge, SouthBridge
- * Description: Map the address space into separated devices.
- */
 `default_nettype none
-`include "include/memconfig.v"
+`include "include/instructions.v"
+`include "include/exception.v"
+`include "include/memory.v"
 
 /*
- * Module Name: NorthBridge
- * Description: Connect CPU and Memory
+ *  Overview: Bridge module, connect CPU with memory and devices
+ *      CPU side: I Port (Read only), D Port (Read and Write) and Interrupt Port
  */
-module NorthBridge (
-    // CPU Side
-    // RW Port 1 (For IM)
-    input wire [31:0] Addr1, 
-    input wire [31:0] WData1, 
-    input wire [3:0] WE1, 
-    output wire [31:0] RData1,
-    // RW Port 2 (For DM)
-    input wire [31:0] PC2, 
-    input wire [31:0] Addr2, 
-    input wire [31:0] WData2, 
-    input wire [3:0] WE2, 
-    output wire [31:0] RData2,
-    // Interruption
-    output wire [7:2] HWINT, 
-    // Outer Side
-    // // IM
-    // output wire [31:0] IM_Addr, 
-    // output wire [31:0] IM_WData, 
-    // output wire [3:0] IM_WE, 
-    // input wire [31:0] IM_RData, 
+module Bridge (
+    /* CPU I Port */
+    input wire `WORD IAddr,
+    output wire `WORD IData,
+    output wire IReady,
+    /* CPU D Port */
+    input wire `WORD DPC,
+    input wire `WORD DAddr,
+    input wire DREn,
+    input wire DWEn,
+    input wire [3:0] DByteEn,
+    input wire `WORD DWData,
+    output wire `WORD DRData,
+    output wire DReady,
+    /* CPU Interrupt */
+    output wire `TYPE_INT HWINT,
+    ///////////////////////////
+    // IM
+    output wire `WORD IM_ADDR,
+    output wire IM_CE,
+    output wire IM_WE,
+    output wire IM_RE,
+    output wire [3:0] IM_BE,
+    output wire `WORD IM_DIN,
+    input wire `WORD IM_DOUT,
+    input wire IM_READY,
     // DM
-    output wire [31:0] DM_PC, 
-    output wire [31:0] DM_Addr, 
-    output wire [31:0] DM_WData, 
-    output wire [3:0] DM_WE, 
-    input wire [31:0] DM_RData,
-    // South Bridge
-    output wire [31:0] SBr_PC, 
-    output wire [31:0] SBr_Addr, 
-    output wire [31:0] SBr_WData, 
-    output wire SBr_WE, 
-    input wire [31:0] SBr_RData,
-    input wire [7:2] SBr_HWINT
+    output wire `WORD DM_PC,
+    output wire `WORD DM_ADDR,
+    output wire DM_CE,
+    output wire DM_WE,
+    output wire DM_RE,
+    output wire [3:0] DM_BE,
+    output wire `WORD DM_DIN,
+    input wire `WORD DM_DOUT,
+    input wire DM_READY,
+    // Timer0
+    output wire [31:2] TIMER0_ADDR,
+    output wire `WORD TIMER0_DIN,
+    output wire TIMER0_WE,
+    input wire `WORD TIMER0_DOUT,
+    input wire TIMER0_INT,
+    // Timer1
+    output wire [31:2] TIMER1_ADDR,
+    output wire `WORD TIMER1_DIN,
+    output wire TIMER1_WE,
+    input wire `WORD TIMER1_DOUT,
+    input wire TIMER1_INT,
+    // Outside
+    input wire EXTERN_INT
 );
+
+    /* I Port <===> IM */
+    assign IM_ADDR = IAddr - `IM_ADDR_START;
+    assign IM_CE = 1;
+    assign IM_WE = 0;
+    assign IM_RE = 1;
+    assign IM_BE = 4'b1111;
+    assign IM_DIN = 0;
+    assign IData = IM_DOUT;
+    assign IReady = IM_READY;
+
+
+    /* D Port */
+    wire DMsel, TIMER0sel, TIMER1sel;
+    assign DMsel = (DAddr >= `DM_ADDR_START && DAddr <= `DM_ADDR_END);
+    assign TIMER0sel = (DAddr >= `TIMER0_ADDR_START && DAddr <= `TIMER0_ADDR_END);
+    assign TIMER1sel = (DAddr >= `TIMER1_ADDR_START && DAddr <= `TIMER1_ADDR_END);
+
     // DM
-    assign DM_PC = PC2;
-    assign DM_Addr = Addr2;
-    assign DM_WData = WData2;
-    
-    assign DM_WE = (Addr2 >= `DM_ADDR_START && Addr2 < `DM_ADDR_END) ? (WE2) : 4'b0;
+    assign DM_PC = DPC;
+    assign DM_ADDR = DAddr;
+    assign DM_DIN = DWData;
+    assign DM_BE = DByteEn;
+    assign DM_CE = DMsel & (DWEn | DREn);
+    assign DM_WE = (DWEn) & (DM_CE);
+    assign DM_RE = (DREn) & (DM_CE);
 
-    // South Bridge
-    assign SBr_PC = PC2;
-    assign SBr_Addr = Addr2;
-    assign SBr_WData = WData2;
-    assign SBr_WE = (
-        (Addr2 >= `TIMER0_ADDR_START && Addr2 < `TIMER0_ADDR_END) ||
-        (Addr2 >= `TIMER1_ADDR_START && Addr2 < `TIMER1_ADDR_END)
-    ) ? (&WE2) : 0;
+    // Timer0
+    assign TIMER0_ADDR = DAddr[31:2];
+    assign TIMER0_DIN = DWData;
+    assign TIMER0_WE = TIMER0sel & DWEn;
 
-    assign RData2 = (Addr2 >= `DM_ADDR_START && Addr2 < `DM_ADDR_END) ? (DM_RData) : 
-                    (Addr2 >= `TIMER0_ADDR_START && Addr2 < `TIMER0_ADDR_END) ? (SBr_RData) : 
-                    (Addr2 >= `TIMER1_ADDR_START && Addr2 < `TIMER1_ADDR_END) ? (SBr_RData) : 
-                    0 ;
-    assign HWINT = SBr_HWINT;
+    // Timer1
+    assign TIMER1_ADDR = DAddr[31:2];
+    assign TIMER1_DIN = DWData;
+    assign TIMER1_WE = TIMER1sel & DWEn;
 
-endmodule
+    // D-MUX
+    assign {DRData, DReady} =   (DMsel & DREn) ? ({DM_DOUT, DM_READY}) :
+                                (TIMER0sel & DREn) ? ({TIMER0_DOUT, 1'b1}) :
+                                (TIMER1sel & DREn) ? ({TIMER1_DOUT, 1'b1}) :
+                                0;
 
-/*
- * Module Name: SouthBridge
- * Description: Connect CPU and Devices
- */
+    /* Interrupt */
+    assign HWINT = {3'b0, EXTERN_INT, TIMER1_INT, TIMER0_INT};
 
-module SouthBridge (
-    // CPU Side
-    input wire [31:0] Addr, 
-    input wire [31:0] WData, 
-    input wire WE,  
-    output wire [31:0] RData, 
-    output wire [7:2] HWINT, 
-    // Device Side
-    // Timer 0
-    output wire [31:2] Timer0_Addr, 
-    output wire [31:0] Timer0_WData, 
-    output wire Timer0_WE, 
-    input wire [31:0] Timer0_RData, 
-    input wire Timer0_Int, 
-    // Timer 1
-    output wire [31:2] Timer1_Addr, 
-    output wire [31:0] Timer1_WData, 
-    output wire Timer1_WE, 
-    input wire [31:0] Timer1_RData, 
-    input wire Timer1_Int, 
-    // External Interrupt
-    input wire Ext_Int
-);
-    assign Timer0_Addr = Addr[31:2];
-    assign Timer0_WData = WData;
-    assign Timer0_WE = (Addr >= `TIMER0_ADDR_START && Addr < `TIMER0_ADDR_END) ? (WE) : 0;
-
-    assign Timer1_Addr = Addr[31:2];
-    assign Timer1_WData = WData;
-    assign Timer1_WE = (Addr >= `TIMER1_ADDR_START && Addr < `TIMER1_ADDR_END) ? (WE) : 0;
-
-    assign RData = (Addr >= `TIMER0_ADDR_START && Addr < `TIMER0_ADDR_END) ? (Timer0_RData) : 
-                    (Addr >= `TIMER1_ADDR_START && Addr < `TIMER1_ADDR_END) ? (Timer1_RData) : 
-                    0;
-
-    assign HWINT = {3'b0, Ext_Int, Timer1_Int, Timer0_Int};
 endmodule
